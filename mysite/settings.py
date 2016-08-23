020f104d2a427f386efca4947f805d3cacd4fd99
Luke Zhu <lukezhu@Lukes-MacBook-Pro.local>
2016-08-22 22:14:35 -0700

    remove statidfiles_dir

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,16 +128,15 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
+"""
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
+"""
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
