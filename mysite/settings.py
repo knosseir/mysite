fa3aa19cbc2e0f2b21eecd59e87bf53a24fb23a4
Luke Zhu <lukezhu@Lukes-MacBook-Pro.local>
2016-08-22 22:25:31 -0700

    set to sqlite3

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,7 +136,17 @@ STATICFILES_DIRS = (
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 
