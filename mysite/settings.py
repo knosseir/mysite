1c799510fd0c031b32dfefa7e976500096ad6cec
Luke Zhu <lukezhu@Lukes-MacBook-Pro.local>
2016-08-22 22:41:47 -0700

    added psycopg2

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,17 +136,7 @@ STATICFILES_DIRS = (
 
 # Update database configuration with $DATABASE_URL.
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 
